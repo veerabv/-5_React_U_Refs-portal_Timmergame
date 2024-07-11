f3bfa447bfa36fc39340519d421a66da2fdcca8a
veerabv <bveera0608@gmail.com>
2024-07-11 21:32:39 +0530

    adding the timmer using useRef VS normal variable and the issue with the variable

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,17 +1,35 @@
-import React from "react";
-
+import React, { useState, useRef } from "react";
+// let timer;
 const TimerChallenge = ({ title, targetTime }) => {
+  const [timeExpired, setTimeExpired] = useState(false);
+  const [timerStarted, setTimerStarted] = useState(false);
+  //   let timer;
+  const timer = useRef();
+  function handleStart() {
+    setTimerStarted(true);
+    timer.current = setTimeout(() => {
+      setTimeExpired(true);
+    }, targetTime * 1000);
+  }
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
   return (
     <section className="challenge">
       <h2>{title}</h2>
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
+      {timeExpired && <p>You lost!!</p>}
       <p>
-        <button>start challenge</button>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"} challenge
+        </button>
       </p>
       <p>
-        <button>Timer is Running ... / Timer Inactive</button>
+        <button>
+          {timerStarted ? "Timer is Running ..." : "Timer Inactive"}{" "}
+        </button>
       </p>
     </section>
   );
