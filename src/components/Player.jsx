1371d57124d8375ce08c0b46266bb5ea84ca7575
veerabv <bveera0608@gmail.com>
2024-07-11 11:08:52 +0530

    Player component changed from useState to useRef

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,22 +1,17 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
-  const [playerName, setPlayerName] = useState("");
-  const [isSubmitted, setIsSubmitted] = useState(false);
-  function handleChange(event) {
-    setIsSubmitted(false)
-    setPlayerName(event.target.value);
-  }
-
+  let playerName = useRef();
+  const [enteredPlayerName, setEnterdPlayerName] = useState(null);
   function handleSubmit() {
-    setIsSubmitted(true);
+    setEnterdPlayerName(playerName.current.value);
   }
 
   return (
     <section id="player">
-      <h2>Welcome { isSubmitted ? playerName :"unknown entity"}</h2>
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" onChange={handleChange} value={playerName} />
+        <input ref={playerName} type="text" />
         <button onClick={handleSubmit}>Set Name</button>
       </p>
     </section>
